b859a0094e5e5d6ff531d9379f09a6ba11bf8536
David Fariña <david.farina@decentage.com>
2021-04-06 17:07:17 +0200

    change connect options

--- main.js
+++ main.js
@@ -19,21 +19,25 @@ const request = require('request');
 config = require('./config.js');
 
 mongoose.Promise = global.Promise;
-mongoose.connect(
-    config.mongodb.host,
-    {
-        user:config.mongodb.user,
-        pass:config.mongodb.pass
 
-        //   auth:{
-        //		authdb:'admin',
-        //		useMongoClient: true,
-        //		user:config.mongodb.user,
-        //		pass:config.mongodb.pass
-        //  }
+const {
+  MONGO_USERNAME,
+  MONGO_PASSWORD,
+  MONGO_HOSTNAME,
+  MONGO_PORT,
+  MONGO_DB
+} = process.env;
 
-    }
-);
+const options = {
+  useNewUrlParser: true,
+  reconnectTries: Number.MAX_VALUE,
+  reconnectInterval: 500,
+  connectTimeoutMS: 10000,
+};
+
+const url = `mongodb://${MONGO_USERNAME}:${MONGO_PASSWORD}@${MONGO_HOSTNAME}:${MONGO_PORT}/${MONGO_DB}?authSource=admin`;
+
+mongoose.connect(url, options);
 
 var express = require('express');
 var app = express();
