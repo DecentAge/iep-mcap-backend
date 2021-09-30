55963eaa90bec1eb1609f19c71027baac9c3c130
Marco Oesch <marco.oesch@decentage.com>
2021-09-30 15:30:20 +0200

    fixed build script

--- build-docker.sh
+++ build-docker.sh
@@ -5,8 +5,8 @@ set -o nounset
 
 RELEASE_VERSION=$(cat release-version.txt)
 docker build -t decentage/iep-mcap-backend:${RELEASE_VERSION} .
-
+docker container rm --force iep-mcap-backend-extr
 CONTAINER_ID=$(docker create --rm --name iep-mcap-backend-extr decentage/iep-mcap-backend:${RELEASE_VERSION})
 mkdir -p ./build
-docker cp ${CONTAINER_ID}:/build/iep-mcap-backend.zip ./build
+docker cp ${CONTAINER_ID}:/build/iep-mcap-backend.zip ./build || echo "Could not copy /build/iep-mcap-backend.zip from container"
 docker rm ${CONTAINER_ID}
