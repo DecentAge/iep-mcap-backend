51b4a8ccf84c0940e347184fd9bcb510781c549c
DecentAgeCoder <marco.oesch@decentage.com>
2021-11-29 17:19:40 +0100

    updated

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,9 +4,9 @@ set -eou pipefail
 init_secret() {
 	local secret_name="$1"
 	if [[ -f "/run/secrets/${secret_name}" ]]; then
-		echo "Initializing secret ${secret_name} from secret /run/secrets/${secret_name}"
+		echo "Initializing secret "${secret_name}" from secret /run/secrets/${secret_name}"
 		local secret_value=$(cat /run/secrets/${secret_name})
-		export ${secret_name}=${secret_value}
+		export ${secret_name}="${secret_value}"
 	elif [[ -n ${secret_name:-} ]]; then
 		echo "Initializing secret ${secret_name} from variable ${secret_name}"
 	else
